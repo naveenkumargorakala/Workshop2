c257c4df8f04f1b067b23198bb6a294fe1607694
Naveen <naveengorakala814@gmail.com>
2023-03-16 11:09:19 +0530

    Add:Workshop2's 2nd problem

---
+++ src/Hospitals/HospitalRecords.java
@@ -0,0 +1,12 @@
+package Hospitals;
+import java.util.Scanner;
+public class HospitalRecords {
+    Patient patient;
+    public void addPatient(){
+
+        Scanner input = new Scanner(System.in)
+        System.out.println("Name: ");
+        patient.name= input.next();
+
+    }
+}
